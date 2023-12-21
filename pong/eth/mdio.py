ce6a116f77e7f61a529c876bca9fb35b97bb68ff
Piotr Wegrzyn <piotro@piotro.eu>
2023-12-21 14:46:00 +0100

    MDIO works fr fr

--- pong/eth/mdio.py
+++ pong/eth/mdio.py
@@ -6,39 +6,46 @@ from transactron import *
 
 PHY_ETH0 = 0b10000
 REG_LED_OVERRIDE = 25
+REG_AUTO_NEG_ADV = 4
+REG_EXT_ADDR = 22
+REG_1000T_CTRL = 9
 
 MDIO_WRITE_LAYOUT = [("addr", 5), ("reg", 5), ("data", 16)]
 
 class MDIOController(Elaboratable):
     def __init__(self):
         self.o_mdc = Signal()
-        self.o_mdio = Signal()
+        self.o_mdio = Signal(reset=1)
+        self.oe_mdio = Signal(reset=0)
         
         self.write = Method(i=MDIO_WRITE_LAYOUT)
 
     def elaborate(self, platform):
         m = TModule()
         
-        tx_word = Signal(32)
-        tx_cnt = Signal(range(32+1))
+        tx_word = Signal(32+33)
+        tx_cnt = Signal(range(32+32))
 
         active = Signal()
         
-        m.submodules.clkdiv = clkdiv = ClkFlag(4) # 3.125 MHz
+        m.submodules.clkdiv = clkdiv = ClkFlag(5) 
         with m.If(clkdiv.tick & active):
             m.d.sync += self.o_mdc.eq(~self.o_mdc)
             
             with m.If(self.o_mdc):
-                idx = (31-tx_cnt).as_unsigned()
-                m.d.sync += self.o_mdio.eq(Mux(tx_cnt == 32, 0, (tx_word >> idx) & 1))
+                idx = (31+32-tx_cnt).as_unsigned()
+                m.d.sync += self.o_mdio.eq(Mux(tx_cnt == 32+32, 1, (tx_word.bit_select(idx,1)) & 1))
+                m.d.sync += self.oe_mdio.eq((tx_cnt != 32+32))
+
                 m.d.sync += tx_cnt.eq(tx_cnt + 1)
                 
-                with m.If(tx_cnt == 32):
+                with m.If(tx_cnt == 32+32):
                     m.d.sync += active.eq(0)
+                    m.d.sync += tx_cnt.eq(0)
         
         @def_method(m, self.write, ready=~active)
         def _(addr, reg, data):
-            m.d.sync += tx_word.eq(Cat(data, C(0), C(1), reg, addr, C(0b01, 2), C(0b01, 2)))
+            m.d.sync += tx_word.eq(Cat(data, C(0), C(1), reg, addr, C(0b01, 2), C(0b01, 2), C(2**32-1,32)))
             m.d.sync += active.eq(1)
 
         return m
--- pong/pong.py
+++ pong/pong.py
@@ -2,7 +2,7 @@ from amaranth import *
 
 from transactron.core import TModule, Transaction 
 
-from pong.eth.mdio import MDIOController
+from pong.eth.mdio import PHY_ETH0, REG_1000T_CTRL, REG_AUTO_NEG_ADV, REG_EXT_ADDR, MDIOController
 
 class Pong(Elaboratable):
     def __init__(self):
@@ -15,13 +15,31 @@ class Pong(Elaboratable):
             enet = platform.request("enet")
             m.d.comb += enet.mdc.eq(self.mdio.o_mdc)
             m.d.comb += enet.mdio.o.eq(self.mdio.o_mdio)
-            m.d.comb += enet.mdio.oe.eq(1)
+            m.d.comb += enet.mdio.oe.eq(1)#self.mdio.oe_mdio)
+            m.d.comb += enet.rst.eq(ResetSignal())
         
-        init = Signal()
-        with Transaction(name = "init").body(m, request=~init):
-            self.mdio.write(m, addr=0b10000, reg=25, data=2**10-1) # LED OVERRIDE TEST
-            m.d.sync += init.eq(1)
-        
+        init = Signal(reset=1)
+        with m.FSM("START"):
+            with m.State("START"):
+                with Transaction().body(m):
+                    self.mdio.write(m, addr=PHY_ETH0, reg=REG_1000T_CTRL, data=0) # disable 1 Gig negotiation
+                    #self.mdio.write(m, addr=PHY_ETH0, reg=25, data=0xffff) # LED OVERRIDE
+                    m.next = "INIT1"
+            with m.State("INIT1"):
+                with Transaction().body(m):
+                    #self.mdio.write(m, addr=PHY_ETH0, reg=REG_AUTO_NEG_ADV, data=(1<<7)|(1<<8)) # enable only 100 BASET negotiation
+                    m.next = "INIT2"
+            with m.State("INIT2"):
+                with Transaction().body(m):
+                    #self.mdio.write(m, addr=PHY_ETH0, reg=25, data=0xff) # LED OVERRIDE
+                    m.next = "INIT_RESET"
+            with m.State("INIT_RESET"):
+                with Transaction().body(m):
+                    #self.mdio.write(m, addr=PHY_ETH0, reg=0, data=(1<<15)) # soft reset 
+                    m.next = "END"
+            with m.State("END"):
+                m.d.sync += init.eq(0)
+
         if platform is not None:
             m.d.comb += platform.request("led_r").eq(1)
 
